10685ee32c5db0d62cf834937af1ce1fb9b1e4d4
Andrei Macavei <andrei.macavei@endava.com>
2016-10-23 23:57:39 +0300

    get account info

--- src/main/webapp/app/components/commons/urlConstants.js
+++ src/main/webapp/app/components/commons/urlConstants.js
@@ -3,5 +3,6 @@
 angular
   .module('Dissertation')
   .constant('urlConstants', {
-    AUTHENTICATE: 'http://localhost:8080/Dissertation-1.0-SNAPSHOT/authenticate'
+    AUTHENTICATE: 'http://localhost:8080/Dissertation-1.0-SNAPSHOT/authenticate',
+    ACCOUNT: 'http://localhost:8080/Dissertation-1.0-SNAPSHOT/security/account'
   })
