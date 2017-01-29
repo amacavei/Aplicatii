fbf366dc5f1d9a2711e03bcb98055a402529e3be
Andrei Macavei <andrei.macavei@endava.com>
2017-01-29 19:46:31 +0200

    Fixed loading state and reload bugs

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -100,26 +100,29 @@ angular
   }])
   .run([
     '$rootScope',
-    '$location',
+    '$state',
     '$http',
     'AuthSharedService',
     'Session',
     'USER_ROLES',
     '$q',
     '$timeout',
-    function($rootScope,$location,$http,AuthSharedService,Session,USER_ROLES,$q,$timeout){
-      $rootScope.$on('routeChangeStart', function(event,next){
-        if(next.originalPath === '/login' && $rootScope.authenticated){
+    function($rootScope, $state, $http, AuthSharedService, Session, USER_ROLES, $q, $timeout){
+
+    $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams){
+        if(toState.url === '/login' && $rootScope.authenticated){
           event.preventDefault();
-        }else if(next.access && next.access.loginRequired && !$rootScope.authenticated){
+        }else if(toState.access && toState.access.loginRequired && !$rootScope.authenticated){
           event.preventDefault();
           $rootScope.$broadcast("event:auth-loginRequired", {});
-        }else if(next.access && !AuthSharedService.isAuthorized(next.access.authorizedRoles)){
+        }else if(toState.access && !AuthSharedService.isAuthorized(toState.access.authorizedRoles)){
           event.preventDefault();
           $rootScope.$broadcast("event:auth-forbidden", {});
         }
       });
-      $rootScope.$on('$routeChangeSuccess', function(scope, next, current){
+
+      $rootScope.$on('$stateChangeSuccess', function(scope, next, current){
+
         $rootScope.$evalAsync(function(){
           $.material.init();
         })
@@ -128,14 +131,14 @@ angular
     $rootScope.$on('event:auth-loginConfirmed', function (event, data) {
         console.log('login confirmed start ' + data);
         $rootScope.loadingAccount = false;
-        var nextLocation = ($rootScope.requestedUrl ? $rootScope.requestedUrl : "/home");
-        var delay = ($location.path() === "/loading" ? 1500 : 0);
+        var nextLocation = ($rootScope.requestedUrl ? $rootScope.requestedUrl : "home");
+        var delay = ($state.current.name === "loading" ? 1500 : 0);
 
         $timeout(function () {
             Session.create(data);
             $rootScope.account = Session;
             $rootScope.authenticated = true;
-            $location.path(nextLocation).replace();
+            $state.go(nextLocation);
         }, delay);
 
     });
@@ -143,26 +146,26 @@ angular
     // Call when the 401 response is returned by the server
     $rootScope.$on('event:auth-loginRequired', function (event, data) {
         if ($rootScope.loadingAccount && data.status !== 401) {
-            $rootScope.requestedUrl = $location.path();
-            $location.path('/loading');
+            $rootScope.requestedUrl = $state.current.name;
+            $state.go('loading');
         } else {
             Session.invalidate();
             $rootScope.authenticated = false;
             $rootScope.loadingAccount = false;
-            $location.path('/login');
+            $state.go('login');
         }
     });
 
     // Call when the 403 response is returned by the server
     $rootScope.$on('event:auth-forbidden', function (rejection) {
         $rootScope.$evalAsync(function () {
-            $location.path('/error/403').replace();
+            $state.go('errorCode',{code: 403});
         });
     });
 
     // Call when the user logs out
     $rootScope.$on('event:auth-loginCancelled', function () {
-        $location.path('/login').replace();
+        $state.go('login');
     });
 
     // Get already authenticated user account
--- src/main/webapp/components/createUser/createUser.service.js
+++ src/main/webapp/components/createUser/createUser.service.js
@@ -3,19 +3,19 @@
  */
 angular.module("Dissertation")
     .factory('createUserService', ['$http','urlConstants', function($http,urlConstants){
-        var createUser = function(user) {
-                var config = {
-                };
-                $http.post(urlConstants.USERS, $.param({
-                    user:user
-                }), config)
-                    .success(function (data) {
-                        console.log('success', data);
-                    })
-                    .error(function (data, status) {
-                        console.error("error", status);
-                    });
+        function createUser(user) {
+            var config = {
             };
+            $http.post(urlConstants.USERS, $.param({
+                user:user
+            }), config)
+                .success(function (data) {
+                    console.log('success', data);
+                })
+                .error(function (data, status) {
+                    console.error("error", status);
+                });
+        }
 
         return {
             createUser: createUser
